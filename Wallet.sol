86eadaab35895d53662dfd04282cddaf5971850a
tomerbabila <tomer.babila@gmail.com>
2021-09-20 14:05:42 +0300

    Add transfers

--- Wallet.sol
+++ Wallet.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.0;
 contract Wallet {
     address[] public approvers;
     uint256 public quorum;
+    struct Transfer {
+        uint256 id;
+        uint256 amount;
+        address payable to;
+        uint256 approvals;
+        bool sent;
+    }
+    Transfer[] public transfers;
 
     constructor(address[] memory _approvers, uint256 _quorum) public {
         approvers = _approvers;
@@ -12,5 +20,9 @@ contract Wallet {
 
     function getApprovers() external view returns (address[] memory) {
         return approvers;
+    }
+
+    function createTransfer(uint256 amount, address payable to) external {
+        transfers.push(Transfer(transfers.length, amount, to, 0, false));
     }
 }
