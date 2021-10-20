f9ba95a846926d3edc4003ec659fb9d4f485175f
tomerbabila <tomer.babila@gmail.com>
2021-10-21 01:11:01 +0300

    Add test
    Should create transfers

--- test/wallet.test.js
+++ test/wallet.test.js
@@ -23,4 +23,15 @@ contract('Wallet', (accounts) => {
     assert(approvers[2] === accounts[2]);
     assert(quorum.toNumber() === 2);
   });
+
+  it('should create transfers', async () => {
+    await wallet.createTransfer(100, accounts[3], { from: accounts[0] });
+    const transfers = await wallet.getTransfers();
+    assert(transfers.length === 1);
+    assert(transfers[0].id === '0');
+    assert(transfers[0].amount === '100');
+    assert(transfers[0].to === accounts[3]);
+    assert(transfers[0].approvals === '0');
+    assert(transfers[0].sent === false);
+  });
 });
