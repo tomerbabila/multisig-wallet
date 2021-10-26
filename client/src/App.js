8ed72ace1030e1368a7e26070cfdf597fb5fdddf
tomerbabila <tomer.babila@gmail.com>
2021-10-27 01:48:25 +0300

    Initialize web3, accounts and wallet

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getWallet, getWeb3 } from './utils';
 
 function App() {
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState([]);
+  const [wallet, setWallet] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const wallet = await getWallet(web3);
+
+      setWeb3(web3);
+      setAccounts(accounts);
+      setWallet(wallet);
+    };
+    init();
+  }, []);
+
   return <div>Multisig Wallet</div>;
 }
 
