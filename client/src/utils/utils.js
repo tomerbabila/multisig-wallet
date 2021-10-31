8572ab521c0c4a36805ba2e44f7863927d486c2f
tomerbabila <tomer.babila@gmail.com>
2021-11-01 00:27:46 +0200

    Connect properly to web3

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,8 +1,24 @@
 import Web3 from 'web3';
 import Wallet from '../contracts/Wallet.json';
 
-const getWeb3 = () => {
-  return new Web3('http://localhost:9545');
+const getWeb3 = async () => {
+  return new Promise((resolve, reject) => {
+    window.addEventListener('load', () => {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          window.ethereum.enable();
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      } else if (window.web3) {
+        resolve(window.web3);
+      } else {
+        reject('Must install Metamask');
+      }
+    });
+  });
 };
 
 const getWallet = async (web3) => {
